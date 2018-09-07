65365d1ebbd7d2f64610535f8443b8b0dc6b4199
9iv <42171095+9iv@users.noreply.github.com>
2018-09-07 12:41:38 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -287,6 +287,22 @@ if (message.content.startsWith(prefix + 'setplay')) {
 });
 
 
+client.on('message', message => {
+  if(message.content.startsWith(prefix + "invites")) {
+   message.guild.fetchInvites().then(invs => {
+     let user = message.mentions.users.first() || message.author
+     let personalInvites = invs.filter(i => i.inviter.id === user.id);
+     let inviteCount = personalInvites.reduce((p, v) => v.uses + p, 0);
+              let mmmmEmbed = new Discord.RichEmbed()
+                        .setAuthor(client.user.username)
+                        .setThumbnail(message.author.avatarURL)
+.addField(` لقد قمت بدعوة :`, ` ${inviteCount} `)
+          .setFooter(`- Requested By: ${message.author.tag}`);
+          message.channel.send(mmmmEmbed)
+});
+ }
+});
+
 client.on('message', vaniet => {
 
 if (ra3d.content ===  prefix + 'create-colors'){
