d958f2dcf7f6b26639225b56389bad01e07d2aca
9iv <42171095+9iv@users.noreply.github.com>
2018-09-07 17:33:53 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -636,5 +636,46 @@ client.on('message', async message => {
   }
 });
 
+const hastebin = require('hastebin-gen');
+client.on('message', message => {
+var PREFIX = 'البرفكس';
+    if(message.content.startsWith(PREFIX + 'dis')) {
+            var args = message.content.split(' ').slice(1).join(' ');
+}
+      var array = [];
+      var i = 0;
+      if(args){
+client.users.filter(u => u.discriminator == args).map(u => {
+    if(i > 4){
+     return;
+    }
+    i = i + 1;
+
+   array.push(`${u.tag}`);
+});
+}
+hastebin(`${array.slice(0, 30).join('\n')}`, 'txt').then(l => {
+    message.channel.send(`${l}`);
+}).catch(console.error);
+});
+client.on('message' , message => {
+var PREFIX = 'البرفكس';
+if(message.content === `${PREFIX}dis`) {
+                      let array = [];
+                      var i = 0;
+client.users.filter(u => u.discriminator == message.author.discriminator).map(u => {
+    if(i > 4){
+     return;
+    }
+    i = i + 1;
+   array.push(`${u.tag}`);
+});
+hastebin(`${array.slice(0, 30).join('\n')}`, 'txt').then(l => {
+    message.channel.send(`${l}`);
+}).catch(console.error);
+
+        }
+});
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
