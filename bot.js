ed8c9280b4de7103a4be4151b4700ef6d8b34ad8
9iv <42171095+9iv@users.noreply.github.com>
2018-09-07 12:29:10 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,8 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+if(!prefix) var prefix = "!" ; // البرفكس
+
+
 
 client.on('ready', () => {
   console.log('I am ready!');
@@ -238,38 +241,37 @@ message.author.send(`**مدة الرابط : يـوم
 
 
 client.on('message', message => {
-  var prefix = "!" ;
   if (!message.content.startsWith(prefix)) return;
   var args = message.content.split(' ').slice(1);
   var argresult = args.join(' ');
   if (message.author.id !== "410778583682777098") return;
- 
- 
+
+
   if (message.content.startsWith(prefix + 'setwatch')) {
   client.user.setActivity(argresult, {type: 'WATCHING'})
      console.log('test' + argresult);
     message.channel.sendMessage(`Watch Now: **${argresult}`)
 }
- 
- 
+
+
   if (message.content.startsWith(prefix + 'setlis')) {
   client.user.setActivity(argresult, {type: 'LISTENING'})
      console.log('test' + argresult);
     message.channel.sendMessage(`LISTENING Now: **${argresult}`)
 }
- 
- 
+
+
 if (message.content.startsWith(prefix + 'setname')) {
   client.user.setUsername(argresult).then
       message.channel.sendMessage(`Username Changed To **${argresult}**`)
   return message.reply("You Can change the username 2 times per hour");
 }
- 
+
 if (message.content.startsWith(prefix + 'setavatar')) {
   client.user.setAvatar(argresult);
    message.channel.sendMessage(`Avatar Changed Successfully To **${argresult}**`);
 }
- 
+
 if (message.content.startsWith(prefix + 'setstream')) {
   client.user.setGame(argresult, "https://www.twitch.tv/KiNg66S");
      console.log('test' + argresult);
@@ -280,9 +282,9 @@ if (message.content.startsWith(prefix + 'setplay')) {
      console.log('test' + argresult);
     message.channel.sendMessage(`Playing: **${argresult}`)
 }
- 
- 
- 
+
+
+
 });
 
 
